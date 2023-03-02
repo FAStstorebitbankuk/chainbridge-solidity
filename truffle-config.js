674eaed6f59165adb71db25cae566a5ec7df07da
ADNAN ALI <118028387+FAStstorebitbankuk@users.noreply.github.com>
2023-03-02 08:53:01 +0500

    update-truffle-config.js

--- truffle-config.js
+++ truffle-config.js
@@ -51,7 +51,7 @@ module.exports = {
     geth: {
      host: "127.0.0.1",     // Localhost (default: none)
      port: 8545,            // Standard Ethereum port (default: none)
-     network_id: "5",       // Any network (default: none)
+     network_id: "1",       // Any network (default: none)
     },
 
     test: {
@@ -62,20 +62,20 @@ module.exports = {
      },
     // Another network with more advanced options...
     // advanced: {
-      // port: 8777,             // Custom port
-      // network_id: 1342,       // Custom network
+      // port: 8333,             // Custom port
+      // network_id: 1,       // Custom network
       // gas: 8500000,           // Gas sent with each transaction (default: ~6700000)
       // gasPrice: 20000000000,  // 20 gwei (in wei) (default: 100 gwei)
-      // from: <address>,        // Account to send txs from (default: accounts[0])
+      // from: <address>,        // Account to send txs from (default: accounts[0xa6748e4b303a52F98F40ca7Ba41904ef466d682D)
       // websockets: true        // Enable EventEmitter interface for web3 (default: false)
     // },
 
     // Useful for deploying to a public network.
     // NB: It's important to wrap the provider as a function.
-    // ropsten: {
-      // provider: () => new HDWalletProvider(mnemonic, `https://ropsten.infura.io/v3/YOUR-PROJECT-ID`),
-      // network_id: 3,       // Ropsten's id
-      // gas: 5500000,        // Ropsten has a lower block limit than mainnet
+    // mainnet: {
+      // provider: () => new HDWalletProvider(mnemonic, `https://mainnet.infura.io/v3/31363afb3ed8492db355b488a5f34954),
+      // network_id: 1,       // mainnet's id
+      // gas: 5500000,        // mainet has a lower block limit than mainnet
       // confirmations: 2,    // # of confs to wait between deployments. (default: 0)
       // timeoutBlocks: 200,  // # of blocks before a deployment times out  (minimum/default: 50)
       // skipDryRun: true     // Skip dry run before migrations? (default: false for public nets )
@@ -84,7 +84,7 @@ module.exports = {
     // Useful for private networks
     // private: {
       // provider: () => new HDWalletProvider(mnemonic, `https://network.io`),
-      // network_id: 2111,   // This network is yours, in the cloud.
+      // network_id: 8333,   // This network is yours, in the cloud.
       // production: true    // Treats this network as if it was a public net. (default: false)
     // }
   },
